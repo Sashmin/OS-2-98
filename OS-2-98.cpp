168a29b594fe2b56e37994da0d157f16552a482d
Sashmin <tolochko.minsk@gmail.com>
2022-11-27 19:14:58 +0300

    added min_max thread

--- OS-2-98.cpp
+++ OS-2-98.cpp
@@ -1,20 +1,71 @@
-// OS-2-98.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <Windows.h>
+#include <stdio.h>
 
-#include <iostream>
+struct ArrayToChange
+{
+	int arr_size;
+	int* array;
+	int min_elem;
+	int max_elem;
+	double average;
+};
+
+DWORD WINAPI min_max(LPVOID array_struct)
+{
+	ArrayToChange* arrToChObjPtr = static_cast<ArrayToChange*>(array_struct);
+	int min = INT_MAX;
+	int max = INT_MIN;
+	int* arrPtr = arrToChObjPtr->array;
+
+	for (int i = 0; i < arrToChObjPtr->arr_size; i++)
+	{
+		Sleep(7);
+		if (arrPtr[i] < min) {
+			min = arrPtr[i];
+		}
+
+		Sleep(7);
+		if (arrPtr[i] > max) {
+			max = arrPtr[i];
+		}
+	}
+
+	printf("max element value is: %d \n", max);
+	printf("min element value is: %d", min);
+
+	arrToChObjPtr->max_elem = max;
+	arrToChObjPtr->min_elem = min;
+
+	return 0;
+}
 
 int main()
 {
-    std::cout << "Hello Wor";
+	ArrayToChange mainArray;
+
+	printf("Enter the size of the array: ");
+	scanf_s("%d", &mainArray.arr_size);
+
+	mainArray.array = new int[mainArray.arr_size];
+
+	for (int i = 0; i < mainArray.arr_size; i++)
+	{
+		printf("Element %d: ", i + 1);
+		scanf_s("%d", &mainArray.array[i]);
+	}
+
+	printf("\n");
+
+	HANDLE hMin_max = CreateThread(
+		nullptr,
+		0,
+		min_max,
+		&mainArray,
+		0,
+		nullptr
+	);
+
+	WaitForSingleObject(hMin_max, INFINITE);
+	CloseHandle(hMin_max);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
