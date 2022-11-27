d6690ba7d8abc22fd0255bb09ea0797265556112
Sashmin <tolochko.minsk@gmail.com>
2022-11-27 19:59:13 +0300

    added average thread

--- OS-2-98.cpp
+++ OS-2-98.cpp
@@ -1,10 +1,11 @@
 #include <Windows.h>
 #include <stdio.h>
+#include <float.h>
 
 struct ArrayToChange
 {
 	int arr_size;
-	int* array;
+	double* array;
 	int min_elem;
 	int max_elem;
 	double average;
@@ -13,9 +14,9 @@ struct ArrayToChange
 DWORD WINAPI min_max(LPVOID array_struct)
 {
 	ArrayToChange* arrToChObjPtr = static_cast<ArrayToChange*>(array_struct);
-	int min = INT_MAX;
-	int max = INT_MIN;
-	int* arrPtr = arrToChObjPtr->array;
+	double min = DBL_MAX;
+	double max = DBL_MIN;
+	double* arrPtr = arrToChObjPtr->array;
 
 	for (int i = 0; i < arrToChObjPtr->arr_size; i++)
 	{
@@ -30,11 +31,29 @@ DWORD WINAPI min_max(LPVOID array_struct)
 		}
 	}
 
-	printf("max element value is: %d \n", max);
-	printf("min element value is: %d", min);
+	printf("max element value is: %.lf \n", max);
+	printf("min element value is: %.lf", min);
 
 	arrToChObjPtr->max_elem = max;
 	arrToChObjPtr->min_elem = min;
+
+	return 0;
+}
+
+DWORD WINAPI average(LPVOID array_struct)
+{
+	ArrayToChange* arrToChObjPtr = static_cast<ArrayToChange*>(array_struct);
+	double sum = 0;
+	double* arrPtr = arrToChObjPtr->array;
+
+	for (int i = 0; i < arrToChObjPtr->arr_size; i++) {
+		sum += arrPtr[i];
+	}
+
+	double average = sum / arrToChObjPtr->arr_size;
+	printf("Average of all elements in the array: %.2lf", average);
+
+	arrToChObjPtr->average = average;
 
 	return 0;
 }
@@ -46,12 +65,12 @@ int main()
 	printf("Enter the size of the array: ");
 	scanf_s("%d", &mainArray.arr_size);
 
-	mainArray.array = new int[mainArray.arr_size];
+	mainArray.array = new double[mainArray.arr_size];
 
 	for (int i = 0; i < mainArray.arr_size; i++)
 	{
 		printf("Element %d: ", i + 1);
-		scanf_s("%d", &mainArray.array[i]);
+		scanf_s("%lf", &mainArray.array[i]);
 	}
 
 	printf("\n");
@@ -67,5 +86,34 @@ int main()
 
 	WaitForSingleObject(hMin_max, INFINITE);
 	CloseHandle(hMin_max);
+
+	printf("\n");
+
+	HANDLE hAverage = CreateThread(
+		nullptr,
+		0,
+		average,
+		&mainArray,
+		0,
+		nullptr
+	);
+
+	WaitForSingleObject(hAverage, INFINITE);
+	CloseHandle(hAverage);
+
+	printf("\n");
+	printf("\n");
+	printf("Processed array: ");
+
+	for (int i = 0; i < mainArray.arr_size; i++)
+	{
+		if (mainArray.array[i] == mainArray.max_elem ||
+			mainArray.array[i] == mainArray.min_elem)
+		{
+			mainArray.array[i] = mainArray.average;
+		}
+
+		printf("%.2lf ", mainArray.array[i]);
+	}
 }
 
